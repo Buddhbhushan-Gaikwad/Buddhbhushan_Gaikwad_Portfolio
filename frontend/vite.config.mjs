31dff3118ba16ac8e22fb2ff1699aad5fdfc2479
Buddhbhushan Suhas Gaikwad <sushantgaikwad044@gmail.com>
2026-02-11 19:14:52 +0530

    Fix formatting in vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: process.env.REACT_APP_API_URL,
-        changeOrigin: true
-      }
+  proxy: {
+    '/api': {
+      target: process.env.REACT_APP_API_URL,
+      changeOrigin: true
     }
   }
+}
 })
