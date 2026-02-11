c6a67f1e4ca632e2b7da83de6ff53dc0ac36ad4a
Buddhbhushan Suhas Gaikwad <sushantgaikwad044@gmail.com>
2026-02-11 19:15:16 +0530

    Remove server proxy configuration
    Removed server proxy configuration from Vite config.

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-  proxy: {
-    '/api': {
-      target: process.env.REACT_APP_API_URL,
-      changeOrigin: true
-    }
-  }
-}
+  plugins: [react()]
 })
